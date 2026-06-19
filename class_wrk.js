e8934f5f96cf9d7561ac96c4e3c2ffbaab4c5ea7
patelrashiii <rashimrj7781@gmail.com>
2026-06-19 23:07:26 +0530

    class_work_8

--- class_wrk.js
+++ class_wrk.js
@@ -1,4 +1,23 @@
-var add3 =(a,b) => console.log(a+b);
- var myinfo = new Array();
-var myinfo1 = Array();
-console.log(myinfo,myinfo1);
+function checkAttendance() {
+    let total = parseInt(document.getElementById("totalClasses").value);
+    let attended = parseInt(document.getElementById("attendedClasses").value);
+
+    if (total <= 0) {
+        document.getElementById("result").innerHTML =
+            "Please enter a valid number of total classes.";
+        return;
+    }
+
+    let percentage = (attended / total) * 100;
+
+    let status;
+    if (percentage >= 75) {
+        status = "Eligible for Exam";
+    } else {
+        status = "Not Eligible for Exam";
+    }
+
+    document.getElementById("result").innerHTML =
+        "<h3>Attendance: " + percentage.toFixed(2) + "%</h3>" +
+        "<h3>Status: " + status + "</h3>";
+}
