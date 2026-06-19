3224dbeeed86d0264b5bb22f17e0ff5410842d6a
patelrashiii <rashimrj7781@gmail.com>
2026-06-19 22:51:32 +0530

    class_work_4

--- class_wrk.js
+++ class_wrk.js
@@ -1,8 +1,8 @@
-var count;
-document.write("Starting Loop"+"<br/>");
-for(count=0;count<10;count++)
-{
-    document.write("Current Count: "+ count);
-    document.write("<br/>");
+var count=0;
+document.write("Starting Loop ");
+
+while(count<10) {
+    document.write("Current Count:"+count+"<br/>");
+    count++;
 }
 document.write("Loop stopped!");
