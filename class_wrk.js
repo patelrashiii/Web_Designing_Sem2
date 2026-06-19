a1ddf20b166d807d4a66f1eced26386d8cd2c8d8
patelrashiii <rashimrj7781@gmail.com>
2026-06-19 22:56:08 +0530

    class_work_6

--- class_wrk.js
+++ class_wrk.js
@@ -1,24 +1,4 @@
-let age=20;
-if(age>=18) {
-    console.log("You are an Adult.");
+function myFunction()
+{
+    alert("Hello World");
 }
-else {
-    console.log("You are a Minor");
-}
-
-var grade='A';
-document.write("Entering switch block<br/>");
-switch(grade){
-    case 'A': document.write("Good job <br/>");
-    break;
-    case 'B': document.write("Pretty good <br/>");
-    break;
-    case 'C': document.write("Passed <br/>");
-    break;
-    case 'D': document.write("Not so good <br/>");
-    break;
-    case 'E': document.write("Failed <br/>");
-    break;
-    default:document.write("Unknown grade <br/>")
-}
-document.write("Existing switch block");
