4c9479ba7e5b02d5999a1d5f60acec923ce30163
patelrashiii <rashimrj7781@gmail.com>
2026-06-19 22:48:12 +0530

    class_work_3

--- class_wrk.js
+++ class_wrk.js
@@ -1,10 +1,8 @@
-const sym1=Symbol(4);
-console.log(sym1);
-const sym2=Symbol(4);
-console.log(sym2);
-if(sym1==sym2){
-    console.log("true")
+var count;
+document.write("Starting Loop"+"<br/>");
+for(count=0;count<10;count++)
+{
+    document.write("Current Count: "+ count);
+    document.write("<br/>");
 }
-else {
-console.log("false")
-}
+document.write("Loop stopped!");
