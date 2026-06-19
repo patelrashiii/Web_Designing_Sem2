4383c8610780604d5814650cd72d303777c117a7
patelrashiii <rashimrj7781@gmail.com>
2026-06-19 22:45:51 +0530

    class_work_2

--- class_wrk.js
+++ class_wrk.js
@@ -1,19 +1,10 @@
-var num=10;
-console.log(num);
-console.log(typeof(num));
-
-num=false;
-console.log(num);
-console.log(typeof(num));
-
-num="Rashi Patel";
-console.log(num);
-console.log(typeof(num));
-
-num=null;
-console.log(num);
-console.log(typeof(num));
-
-num=undefined;
-console.log(num);
-console.log(typeof(num));
+const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true")
+}
+else {
+console.log("false")
+}
